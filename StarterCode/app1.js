50a9e1f43b290fe0b9a6dba9da8647c635350e52
JayD <84251992+jaybdhruv@users.noreply.github.com>
2021-09-08 20:00:24 -0400

    files updated

---
+++ StarterCode/app1.js
@@ -0,0 +1,9 @@
+// const samplesDasta = "samples.json";
+
+// d3.json(samplesData).then(function(data){
+//     console.log(data)
+// });
+
+// d3.json("samples1.json").then((data) => {console.log(data);});
+
+d3.json("samples1.json").then(function(data){ console.log(data)});
---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,9 @@
+// const samplesDasta = "samples.json";
+
+// const dataPromise = d3.json(samplesData);
+
+// d3.json(samplesData).then(function(data){
+//     console.log(data)
+// });
+
+d3.json("samples.json").then((data) => {console.log(data);});
