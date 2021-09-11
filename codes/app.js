2d8f3c6c07f2adced5b3d205fd06f8e99b70ad7a
JayD <84251992+jaybdhruv@users.noreply.github.com>
2021-09-11 09:52:58 -0400

    script updated

--- codes/app.js
+++ codes/app.js
@@ -2,6 +2,17 @@ const samplesData = "samples.json";
 
 d3.json(samplesData).then(function(data){
     console.log(data)
+    console.log(data.names)
+    let sample_data = data.samples
+    
+    let otu_ids = sample_data.map(function(data){
+        return data.otu_ids;
+    });
+    console.log("Otu_ids:", otu_ids)
+
+    let otu_labels = sample_data.map(data => data.otu_labels);
+    console.log("Otu_labels", otu_labels)
+
 });
 
 // d3.json("samples.json").then((data) => {console.log(data);});
--- codes/static/js/app.js
+++ codes/static/js/app.js
@@ -2,6 +2,7 @@ const samplesData = "samples.json";
 
 d3.json(samplesData).then(function(data){
     console.log(data)
+    console.log(data.names)
 });
 
 // d3.json("samples.json").then((data) => {console.log(data);});
