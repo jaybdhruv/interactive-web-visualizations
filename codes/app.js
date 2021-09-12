1ec918d0271c95fd04d6f58489bb55aba6cc64d5
JayD <84251992+jaybdhruv@users.noreply.github.com>
2021-09-12 02:35:55 -0400

    bar graph in progress

--- codes/app.js
+++ codes/app.js
@@ -1,18 +1,41 @@
 const samplesData = "samples.json";
 
 d3.json(samplesData).then(function(data){
-    console.log(data)
-    console.log(data.names)
-    let sample_data = data.samples
-    
-    let otu_ids = sample_data.map(function(data){
-        return data.otu_ids;
-    });
-    console.log("Otu_ids:", otu_ids)
 
-    let otu_labels = sample_data.map(data => data.otu_labels);
-    console.log("Otu_labels", otu_labels)
+    let first_id = data.samples.filter(data => data.id == "940").sort((a,b) => (a-b))[0];
+    console.log("First id:", first_id)
+
+    let top_otu_ids = first_id.otu_ids.slice(0,10).reverse();
+    console.log("Otu_ids:", top_otu_ids);
+
+    let top_otu_labels = first_id.otu_labels.slice(0,10).reverse();
+    console.log("Otu_labels:", top_otu_labels);
+
+    let top_sample_values = first_id.sample_values.slice(0,10).reverse();
+    console.log("Sample_data:", top_sample_values);
+
+    let trace1 = {
+        x: top_sample_values,
+        y: top_otu_ids,
+        // text: reversedData.map(object => object.greekName),
+        // name: "Greek",
+        type: "bar",
+        orientation: "h"
+      };
+    
+    let traceData = [trace1];
+
+    let layout = {
+        // title: "Greek gods search results",
+        // margin: {
+        //   l: 100,
+        //   r: 100,
+        //   t: 100,
+        //   b: 100
+        // }
+      };
+    
+    Plotly.newPlot("bar", traceData, layout);
 
 });
 
-// d3.json("samples.json").then((data) => {console.log(data);});
